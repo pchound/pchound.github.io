9dbab98fe9443426d23d71dd88358523b1417e3b
pchound <82906754+pchound@users.noreply.github.com>
2021-05-29 21:57:34 -0600

    Add files via upload

---
+++ lesson5/js/year_clock.js
@@ -0,0 +1,5 @@
+var rightnow = new Date();
+var year = rightnow.getFullYear();
+year_clock = document.getElementById("year_clock");
+year;
+year_clock.innerHTML = "&copy; " + year + " Joseph Garner <br> Utah | <a href=\"https://www.byui.edu/online\">BYU Idaho Online</a>";
